cd5bbbdaaf429faf91c3495a2450daf95ced6a44
ilee5077 <162933150+ilee5077@users.noreply.github.com>
2024-04-15 10:16:49 +1000

    save state before implementing import/export profit AB trade

--- koreant_algotrading.py
+++ koreant_algotrading.py
@@ -7,7 +7,6 @@
 #[typing](https://docs.python.org/3.9/library/typing.html)
 #[jsonpickle](https://pypi.org/project/jsonpickle/)
 
-
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
@@ -15,6 +14,7 @@ import jsonpickle
 import numpy as np
 import statistics
 import math
+
 
 class Trader:
 
@@ -129,14 +129,10 @@ class Trader:
 		
 		orders: list[Order] = []
 
-		# find buy orders
 		buy_orders = list(state.order_depths[product].buy_orders.items())
-		#print(f"buy_orders: {buy_orders}")
 		best_buy_pr, buy_vol = buy_orders[0]
 
-		# find sell orders
 		sell_orders = list(state.order_depths[product].sell_orders.items())
-		#print(f"sell_orders: {sell_orders}")
 		best_sell_pr, sell_vol = sell_orders[0]
 
 		pos_limt = POSITION_LIMIT[product]
@@ -229,12 +225,194 @@ class Trader:
 				orders.append(Order(product, best_buy_pr, -order_vol))
 				orders.append(Order(product, best_sell_pr, order_vol))
 		
-		print(f"arbi orders {product}:{orders}")
+		#print(f"arbi orders {product}:{orders}")
 		return orders
 
-			
+	def next_orchid_price(self, state: TradingState):
+		# end of timestamp dont trade!
+		product = 'ORCHIDS'
+		intercept = -0.0439889100189248
+		#'ORCHIDS'	'SUNLIGHT'	 'HUMIDITY'	 'diffSUNLIGHT>2500'	 'humdiff'
+		coef = [0.999623941,	-0.0000250973316,	0.0067920161,	0.0000636415177,	-0.0110500552]
+
+		observations = state.observations.conversionObservations[product]
+
+		buy_orders = list(state.order_depths[product].buy_orders.items())
+		best_buy_pr, buy_vol = buy_orders[0]
+
+		sell_orders = list(state.order_depths[product].sell_orders.items())
+		best_sell_pr, sell_vol = sell_orders[0]
+
+
+		sunlight = observations.sunlight
+		humidity = observations.humidity
+		
+		sundiff = max(sunlight-2500,0)
+		humdiff = max(max(humidity-80,0),max(60-humidity,0))
+
+		sum = 0
+		sumvol = 0
+		if len(buy_orders) > 1:
+			for bid,bid_vol in buy_orders[:-1]:
+				sum += bid*bid_vol
+				sumvol += bid_vol
+		else:
+			bid,bid_vol = buy_orders[0]
+			sum += bid*bid_vol
+			sumvol += bid_vol
+		if len(sell_orders) > 1:	
+			for ask,ask_vol in sell_orders[:-1]:
+				sum += ask*-ask_vol
+				sumvol += -ask_vol
+		else:
+			ask,ask_vol = sell_orders[0]
+			sum += ask*ask_vol
+			sumvol += ask_vol
+
+		mprice = sum/sumvol
+
+		observed_values = []
+
+		observed_values.append(mprice)
+		observed_values.append(sunlight)
+		observed_values.append(humidity)
+		observed_values.append(sundiff)
+		observed_values.append(humdiff)
+		
+		nxt_price = int(round(intercept + math.sumprod(coef,observed_values),0))
+		print(f";OC mprice;{(best_buy_pr+best_sell_pr)/2};wmprice;{mprice};nextprice;{nxt_price}")
+		return nxt_price
 	
-	
+	def compute_arb_conversion_order(self, state: TradingState, avg_running_price):
+		product = 'ORCHIDS'
+		observations = state.observations.conversionObservations[product]
+		conversions = None
+		#price we are paying to buy
+		real_ask_price = observations.askPrice + observations.importTariff + observations.transportFees
+		
+		#price we are getting to sell
+		real_bid_price = observations.bidPrice - observations.exportTariff - observations.transportFees
+		
+		if state.position.get(product,0) > 0:
+			if real_bid_price >= avg_running_price:
+				conversions = -state.position[product]
+		
+		if state.position.get(product,0) < 0:
+			if real_ask_price < avg_running_price:
+				conversions = -state.position[product]
+		print(f";real_bid;{real_bid_price};real_ask;{real_ask_price};avg_running_price;{avg_running_price};conversions;{conversions}")
+		return conversions
+
+	def compute_orders_orchids(self, state: TradingState, acc_bid, acc_ask, POSITION_LIMIT, conversions):
+		product = 'ORCHIDS'
+		
+		#if state.position[product] != 0:
+		#	conversion = self.compute_arb_conversion_order()
+
+
+		orders: list[Order] = []
+
+		buy_orders = list(state.order_depths[product].buy_orders.items())
+		best_buy_pr, buy_vol = buy_orders[0]
+
+		sell_orders = list(state.order_depths[product].sell_orders.items())
+		best_sell_pr, sell_vol = sell_orders[0]
+
+		pos_limt = POSITION_LIMIT[product]
+
+		cpos= 0
+		if product in state.position.keys() and conversions is None:
+			cpos = state.position[product]
+
+		mx_with_buy = -1
+		buy_cpos_update = cpos
+		for ask, vol in sell_orders:
+			# MATCHING ASK ORDERS
+            # sell for less than the price we willing to buy OR
+            # sell for same as we want to buy and our position is short
+			if ((ask < acc_bid) or ((cpos<0) and (ask == acc_bid))) and buy_cpos_update < pos_limt:
+				mx_with_buy = max(mx_with_buy, ask) # buy price
+				order_for = min(-vol, pos_limt - cpos) # how many do we buy for
+				buy_cpos_update += order_for
+				assert(order_for >= 0)
+				orders.append(Order(product, ask, order_for))
+
+		cpos= 0
+		if product in state.position.keys() and conversions is None:
+			cpos = state.position[product]
+		sell_cpos_update = cpos
+		# short positioning
+		for bid, vol in buy_orders:
+			# MATCHING BUY ORDERS
+			# someone is willing to buy more than what we ask
+			# we are long and someone buying at price we think
+			if ((bid > acc_ask) or ((cpos>0) and (bid == acc_ask))) and sell_cpos_update > -pos_limt:
+				order_for = max(-vol, -pos_limt-sell_cpos_update)
+				# order_for is a negative number denoting how much we will sell
+				sell_cpos_update += order_for
+				assert(order_for <= 0)
+				orders.append(Order(product, bid, order_for))
+		
+		sum = 0
+		sumvol = 0
+		if len(buy_orders) > 1:
+			for bid,bid_vol in buy_orders[:-1]:
+				sum += bid*bid_vol
+				sumvol += bid_vol
+		else:
+			bid,bid_vol = buy_orders[0]
+			sum += bid*bid_vol
+			sumvol += bid_vol
+		if len(sell_orders) > 1:	
+			for ask,ask_vol in sell_orders[:-1]:
+				sum += ask*-ask_vol
+				sumvol += -ask_vol
+		else:
+			ask,ask_vol = sell_orders[0]
+			sum += ask*ask_vol
+			sumvol += ask_vol
+
+		mprice = int(round(sum/sumvol,0))
+
+		undercut_buy = mprice - 1
+
+		# buy orders
+		if (buy_cpos_update < pos_limt) and (cpos < 0): # short position
+			num = min(200, pos_limt - buy_cpos_update)
+			orders.append(Order(product, min(undercut_buy + 1, acc_bid-1), num))
+			buy_cpos_update += num
+
+		if (buy_cpos_update < pos_limt) and (cpos > 50): # long position
+			num = min(200, pos_limt - buy_cpos_update)
+			orders.append(Order(product, min(undercut_buy - 1, acc_bid-1), num))
+			buy_cpos_update += num
+
+		if buy_cpos_update < pos_limt: # position between 1 to 14
+			num = min(200, pos_limt - buy_cpos_update)
+			orders.append(Order(product, min(undercut_buy, acc_bid-1), num))
+			buy_cpos_update += num
+
+		undercut_sell = mprice + 1
+		# placing orders
+		if (sell_cpos_update > -pos_limt) and (cpos > 0): # long position
+			num = max(-200, -pos_limt-sell_cpos_update) # sell 40 or 
+			orders.append(Order(product, max(undercut_sell-1, acc_ask+1), num))
+			sell_cpos_update += num
+
+		if (sell_cpos_update > -pos_limt) and (cpos < -50):
+			num = max(-200, -pos_limt-sell_cpos_update)
+			orders.append(Order(product, max(undercut_sell+1, acc_ask+1), num))
+			sell_cpos_update += num
+
+		if sell_cpos_update > -pos_limt:
+			num = max(-200, -pos_limt-sell_cpos_update)
+			orders.append(Order(product, max(undercut_sell, acc_ask+1), num))
+			sell_cpos_update += num
+
+
+
+		return orders
+
 	def run(self, state: TradingState):
 		
 		# Orders to be placed on exchange matching engine
@@ -242,40 +420,38 @@ class Trader:
 
 		if state.traderData == '':
 			mydata = {}
-			mydata['position_limit'] = {'AMETHYSTS' : 20, 'STARFRUIT' : 20}
+			mydata['position_limit'] = {'AMETHYSTS' : 20, 'STARFRUIT' : 20, 'ORCHIDS' : 100}
 			mydata['starfruit_cache'] = []
 			mydata['starfruit_dim'] = 5
+			mydata['last_position'] = {'AMETHYSTS' : 0, 'STARFRUIT' : 0, 'ORCHIDS' : 0}
+			mydata['cpnl'] = {'AMETHYSTS' : 0, 'STARFRUIT' : 0, 'ORCHIDS' : 0}
+			mydata['volume_traded'] = {'AMETHYSTS' : 0, 'STARFRUIT' : 0, 'ORCHIDS' : 0}
+			mydata['running_buy_price'] = 0
+			mydata['running_sell_price'] = 0
+			mydata['running_buy_vol'] = 0
+			mydata['running_sell_vol'] = 0
+			mydata['OC_gap_average'] = 0
 		else:
 			mydata = jsonpickle.decode(state.traderData)
 
 
-
-
 		#COMMENT BELOW WHEN SUBMITTING
-		"""
 		product_keys = [
-			'AMETHYSTS'
-			,'STARFRUIT']
+			#'AMETHYSTS'
+			#,'STARFRUIT']
+   			'ORCHIDS']
 		
 		#Only method required. It takes all buy and sell orders for all symbols as an input,
 		#and outputs a list of orders to be sent
-		want_to_see_product = 'STARFRUIT'
-		print('\n')
-		print(f"timestamp: {state.timestamp}")
+		want_to_see_product = 'ORCHIDS'
+		print(f";timestamp;{state.timestamp}")
 		#print("LISTING")
 		#print(state.listings)
-		print(f"buy_orders: {list(state.order_depths[want_to_see_product].buy_orders.items())}")
-		print(f"sell_orders: {list(state.order_depths[want_to_see_product].sell_orders.items())}")
-		if want_to_see_product in state.own_trades:
-			print(f"own_trades: {state.own_trades[want_to_see_product]}")
-		if want_to_see_product in state.position:
-			print(f"position: {state.position[want_to_see_product]}")
-		if want_to_see_product in state.market_trades:
-			print(f"market_trades {state.market_trades[want_to_see_product]}")
-		#print("traderData: " + state.traderData)
-		#print("Observations: " + str(state.observations))
-		"""
+		print(f";own_trades;{state.own_trades.get(want_to_see_product,"")}")
+		print(f";position;{state.position.get(want_to_see_product,"")}")
+		print(f";market_trades;{state.market_trades.get(want_to_see_product,"")}")
 		
+
 
 		result['AMETHYSTS'] = self.compute_orders_amethysts(state, acc_bid = 10000, acc_ask = 10000, POSITION_LIMIT = mydata['position_limit'])		
 
@@ -293,21 +469,75 @@ class Trader:
 			#nxt_SF_price = statistics.median(mydata['starfruit_cache'])
 			gradient, intercept = np.polyfit(list(range(mydata['starfruit_dim'])),mydata['starfruit_cache'],1)
 			nxt_SF_price2 = int(round(gradient * 6 + intercept,0))
-			print(f"next price SF: {nxt_SF_price}, next price LR: {round(gradient * 6 + intercept,0)},history: {mydata['starfruit_cache']}, gradient: {gradient}")			
+			#print(f"next price SF: {nxt_SF_price}, next price LR: {round(gradient * 6 + intercept,0)},history: {mydata['starfruit_cache']}, gradient: {gradient}")			
 			# now we know the next SF price is going to be - so we can start trade!
 			result['STARFRUIT'] = self.compute_orders_starfruit(state, acc_bid = int(round(nxt_SF_price-1,0)), acc_ask = math.floor(nxt_SF_price+1), POSITION_LIMIT = mydata['position_limit'])
 			
+
+
+		"""
+		if state.position.get('ORCHIDS',0) > 0:
+			mydata['cpnl']['ORCHIDS'] -= state.position.get('ORCHIDS',0) * 0.1
+		#ORCHID conversion pnl update
+		
+		for product in state.own_trades.keys():
+			for trade in state.own_trades[product]:
+				if trade.timestamp != state.timestamp-100:
+					continue
+				mydata['volume_traded'][product] += abs(trade.quantity)
+				if trade.buyer == "SUBMISSION":
+					mydata['cpnl'][product] -= trade.quantity * trade.price
+				else:
+					mydata['cpnl'][product] += trade.quantity * trade.price
+		"""
+
+		if state.position.get("ORCHIDS",0) > 0:
+			mydata['running_buy_price'] += 0.1
+
+		for product in state.own_trades.keys():
+			if product == 'ORCHIDS':
+				for trade in state.own_trades['ORCHIDS']:
+					if trade.timestamp != state.timestamp-100:
+						continue
+					
+					if trade.buyer == "SUBMISSION": 
+						mydata['running_buy_price'] = (mydata['running_buy_price'] * mydata['running_buy_vol'] + trade.quantity * trade.price)/(mydata['running_buy_vol'] + trade.quantity)
+						mydata['running_buy_vol'] += trade.quantity
+					else:
+						mydata['running_sell_price'] = (mydata['running_sell_price'] * mydata['running_sell_vol'] + trade.quantity * trade.price)/(mydata['running_sell_vol'] + trade.quantity)
+						mydata['running_sell_vol'] += trade.quantity
+		
+
+		if (mydata['running_buy_vol'] - mydata['running_sell_vol']) != 0:
+			average_price = round((mydata['running_buy_vol'] * mydata['running_buy_price'] - mydata['running_sell_vol'] * mydata['running_sell_price']) / (mydata['running_buy_vol'] - mydata['running_sell_vol']),1)
+		else:
+			average_price = 0
+
+		conversions = self.compute_arb_conversion_order(state, avg_running_price = average_price)
+
+		nxt_OC_price = self.next_orchid_price(state)
+		result['ORCHIDS'] = self.compute_orders_orchids(state, acc_bid = nxt_OC_price - 1, acc_ask = nxt_OC_price + 1, POSITION_LIMIT = mydata['position_limit'], conversions = conversions)
+		#conversions = None
+		
+		if conversions is not None:
+			mydata['running_buy_price'] = 0
+			mydata['running_sell_price'] = 0
+			mydata['running_buy_vol'] = 0
+			mydata['running_sell_vol'] = 0
+
+		if state.position.get('ORCHIDS',0) > 0:
+			mydata['cpnl']['ORCHIDS'] -= state.position.get('ORCHIDS',0) * 0.1
 		
 		# String value holding Trader state data required. 
 		# It will be delivered as TradingState.traderData on next execution.
+		#for key, item in mydata.items():
+		#	print(key,item)
 		traderData = jsonpickle.encode(mydata) 
-		
-		# Sample conversion request. Check more details below. 
-		conversions = 1
 
 
-		#result = {k: v for k, v in result.items() if k in (product_keys)}
 
-		print(f"order: {result}")
+		result = {k: v for k, v in result.items() if k in (product_keys)}
+
+		print(f";order;{result}")
 		return result, conversions, traderData
 	
---
+++ mlr_orchids.py
@@ -0,0 +1,148 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+import numpy as np
+from sklearn import datasets, linear_model
+import statsmodels.api as sm
+from scipy import stats
+
+# Read the CSV data using pandas, specifying delimiter as ';'
+df_day_0 = pd.read_csv("round-2-island-data-bottle\\prices_round_2_day_-1.csv", delimiter=";")
+df_day_1 = pd.read_csv("round-2-island-data-bottle\\prices_round_2_day_0.csv", delimiter=";")
+df_day_2 = pd.read_csv("round-2-island-data-bottle\\prices_round_2_day_1.csv", delimiter=";")
+
+windows = pd.concat([df_day_0[['SUNLIGHT','HUMIDITY']].shift(n) for n in range(5)], axis=1)
+windows
+df_day_0['SUNLIGHT_5_lag'] = pd.Series(windows.filter(like='SUNLIGHT').values.tolist(), index=df_day_0.index)
+df_day_0['HUMIDITY_5_lag'] = pd.Series(windows.filter(like='HUMIDITY').values.tolist(), index=df_day_0.index)
+
+df_day_0 = df_day_0[4:]
+
+df_day_0['ORCHIDS+1'] = df_day_0['ORCHIDS'].shift(-1)
+df_day_0['ORCHIDS+2'] = df_day_0['ORCHIDS'].shift(-2)
+df_day_0['ORCHIDS+3'] = df_day_0['ORCHIDS'].shift(-3)
+df_day_0['ORCHIDS+5'] = df_day_0['ORCHIDS'].shift(-5)
+df_day_0['ORCHIDS+10'] = df_day_0['ORCHIDS'].shift(-10)
+df_day_0 = df_day_0[~df_day_0['ORCHIDS+10'].isna()]
+
+df_day_0 = df_day_0.reset_index()
+
+df_day_0['SUNGLIGHT_G_5'] = 0
+df_day_0['HUMIDITY_G_5'] = 0
+for i in range(len(df_day_0)):
+    df_day_0['SUNGLIGHT_G_5'][i],intercept = np.polyfit([5,4,3,2,1], df_day_0['SUNLIGHT_5_lag'][i], 1)
+    df_day_0['HUMIDITY_G_5'][i],intercept = np.polyfit([5,4,3,2,1], df_day_0['HUMIDITY_5_lag'][i], 1)
+
+
+windows = pd.concat([df_day_1[['SUNLIGHT','HUMIDITY']].shift(n) for n in range(5)], axis=1)
+windows
+df_day_1['SUNLIGHT_5_lag'] = pd.Series(windows.filter(like='SUNLIGHT').values.tolist(), index=df_day_1.index)
+df_day_1['HUMIDITY_5_lag'] = pd.Series(windows.filter(like='HUMIDITY').values.tolist(), index=df_day_1.index)
+
+df_day_1 = df_day_1[4:]
+
+df_day_1['ORCHIDS+1'] = df_day_1['ORCHIDS'].shift(-1)
+df_day_1['ORCHIDS+2'] = df_day_1['ORCHIDS'].shift(-2)
+df_day_1['ORCHIDS+3'] = df_day_1['ORCHIDS'].shift(-3)
+df_day_1['ORCHIDS+5'] = df_day_1['ORCHIDS'].shift(-5)
+df_day_1['ORCHIDS+10'] = df_day_1['ORCHIDS'].shift(-10)
+df_day_1 = df_day_1[~df_day_1['ORCHIDS+10'].isna()]
+
+df_day_1 = df_day_1.reset_index()
+
+df_day_1['SUNGLIGHT_G_5'] = 0
+df_day_1['HUMIDITY_G_5'] = 0
+for i in range(len(df_day_1)):
+    df_day_1['SUNGLIGHT_G_5'][i],intercept = np.polyfit([5,4,3,2,1], df_day_1['SUNLIGHT_5_lag'][i], 1)
+    df_day_1['HUMIDITY_G_5'][i],intercept = np.polyfit([5,4,3,2,1], df_day_1['HUMIDITY_5_lag'][i], 1)
+
+
+
+windows = pd.concat([df_day_2[['SUNLIGHT','HUMIDITY']].shift(n) for n in range(5)], axis=1)
+windows
+df_day_2['SUNLIGHT_5_lag'] = pd.Series(windows.filter(like='SUNLIGHT').values.tolist(), index=df_day_2.index)
+df_day_2['HUMIDITY_5_lag'] = pd.Series(windows.filter(like='HUMIDITY').values.tolist(), index=df_day_2.index)
+
+df_day_2 = df_day_2[4:]
+
+df_day_2['ORCHIDS+1'] = df_day_2['ORCHIDS'].shift(-1)
+df_day_2['ORCHIDS+2'] = df_day_2['ORCHIDS'].shift(-2)
+df_day_2['ORCHIDS+3'] = df_day_2['ORCHIDS'].shift(-3)
+df_day_2['ORCHIDS+5'] = df_day_2['ORCHIDS'].shift(-5)
+df_day_2['ORCHIDS+10'] = df_day_2['ORCHIDS'].shift(-10)
+df_day_2 = df_day_2[~df_day_2['ORCHIDS+10'].isna()]
+
+df_day_2 = df_day_2.reset_index()
+
+df_day_2['SUNGLIGHT_G_5'] = 0
+df_day_2['HUMIDITY_G_5'] = 0
+for i in range(len(df_day_2)):
+    df_day_2['SUNGLIGHT_G_5'][i],intercept = np.polyfit([5,4,3,2,1], df_day_2['SUNLIGHT_5_lag'][i], 1)
+    df_day_2['HUMIDITY_G_5'][i],intercept = np.polyfit([5,4,3,2,1], df_day_2['HUMIDITY_5_lag'][i], 1)
+
+
+
+
+df_combined = pd.concat([df_day_0, df_day_1, df_day_2])
+df_combined
+
+df_combined["diffSUNLIGHT>2500"] = np.where(df_combined["SUNLIGHT"] > 2500,df_combined["SUNLIGHT"]-2500,0)
+df_combined["humdiff"] = np.where((df_combined["HUMIDITY"]>80), df_combined["HUMIDITY"]-80, np.where((df_combined["HUMIDITY"]<60), 60-df_combined["HUMIDITY"],0))
+
+df_combined["flagSUNLIGHT>2500"] = np.where(df_combined["SUNLIGHT"] > 2500,1,0)
+df_combined["humflag"] = np.where((df_combined["HUMIDITY"]>80), 1, np.where((df_combined["HUMIDITY"]<60), 1,0))
+
+
+# Split the data into independent variables (X) and the dependent variable (Y)
+X = df_combined[['ORCHIDS','SUNLIGHT', 'HUMIDITY', 'diffSUNLIGHT>2500', 'humdiff', 'flagSUNLIGHT>2500', 'humflag']]
+X = df_combined[['ORCHIDS','SUNLIGHT', 'HUMIDITY','SUNGLIGHT_G_5','HUMIDITY_G_5', 'diffSUNLIGHT>2500', 'humdiff']]
+
+X = df_combined[['ORCHIDS','SUNLIGHT', 'HUMIDITY', 'diffSUNLIGHT>2500', 'humdiff']]
+#X = df_combined[['ORCHIDS', 'HUMIDITY', 'diffSUNLIGHT>2500', 'humdiff']]
+Y = df_combined['ORCHIDS+10']
+
+
+# Split the dataset into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=7)
+
+# Create a linear regression model
+model = LinearRegression()
+
+# Fit the model to the training data
+model.fit(X_train, Y_train)
+
+# Make predictions on the test set
+Y_pred = model.predict(X_test)
+
+# Evaluate the model
+print('Mean Absolute Error:', metrics.mean_absolute_error(Y_test, Y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(Y_test, Y_pred))
+print('Root Mean Squared Error:', metrics.mean_squared_error(Y_test, Y_pred, squared=False))
+
+# Print the coefficients and intercept
+print('Coefficients:', model.coef_)
+print('Intercept:', model.intercept_)
+X[:10]
+Y[:10]
+
+df_combined.to_csv('round-2-island-data-bottle\\prices_round_2_combined_mlr.csv')
+
+
+# SIGNIFICANCE TESTING
+X = df[df.columns.difference(['nextORCHIDS','DAY','IMPORT_TARIFF','EXPORT_TARIFF','timestamp'])]
+X = df_combined[['ORCHIDS', 'HUMIDITY', 'SUNLIGHT', 'diffSUNLIGHT>2500', 'humdiff','SUNGLIGHT_G_5','HUMIDITY_G_5']]
+
+X2 = sm.add_constant(X)
+est = sm.OLS(Y, X2)
+est2 = est.fit()
+print(est2.summary())
+
+
+
+
+
+
+
+
+
